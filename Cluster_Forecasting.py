8c49bd4f54b12a70238044649130e770a2b1589d
Twoje imię i nazwisko <w.daszysnki@outlook.com>
2025-05-21 16:04:56 +0200

    complete TODO and move on, major checks

--- Cluster_Forecasting.py
+++ Cluster_Forecasting.py
@@ -1,7 +1,56 @@
 import pandas as pd
 
 # Load data from Excel file into a DataFrame
-dfRawData = pd.read_excel(r"C:\Users\wiktor.daszynski\Downloads\Python\Claster Forecasting Project\Data Cluster Forecasting.xlsx")
-# Display the first few rows of the DataFrame
-print(dfRawData.head(5))
+dfRawData = pd.read_excel(r"C:\Users\wiktor.daszynski\Downloads\Python\Cluster Forecasting Project\Data Cluster Forecasting.xlsx")
 
+#Rename columns heders to english
+dfRawData.rename(columns={
+    "Kod towaru": "Product Code",
+    'Opis towaru': 'Product Name',
+    'Data wystawienia': 'Date of Sale',
+    'Wartość towaru': 'Value',
+    "Cena jednostkowa": "Unit price",
+    "Ilość": "Quantity"
+    }, inplace=True)
+
+### TODO: check price standard deviation per Product code
+
+## ⬇️ To check and rewiew
+# Population standard deviation of Unit price per Product Code
+unit_price_std = dfRawData.groupby('Product Code')['Unit price'].agg(
+    std_pop=lambda x: x.std(ddof=0)
+).reset_index()
+
+print(unit_price_std)
+# ...existing code...
+⬆️
+
+### TODO: Check date of first code sell (min. date when sale >0)
+
+quit()
+
+dfRawData['Date of Sale'] = pd.to_datetime(dfRawData['Date of Sale'], format='%Y-%m-%d')
+
+sale_date_range = pd.date_range(
+    start = "2024.01.01",
+    end = "2025.01.05",
+    freq = "D")
+workdays = sale_date_range[sale_date_range.weekday < 6]
+products = dfRawData['Product Code'].unique()
+
+# full date and unique product code df
+dfFDP = pd.MultiIndex.from_product(
+    [workdays, products],
+    names=['Date of Sale', 'Product Code']
+).to_frame(index=False)
+
+dfMerged = pd.merge(
+    dfFDP,
+    dfRawData,
+    how='left',
+    on=['Date of Sale', 'Product Code'])
+dfMerged["Quantity"] = dfMerged["Quantity"].fillna(0)
+
+### TODO: fill empty cuolumns Name, Unit price, with previous table data -> then calculate Value Q*Price = value
+
+print(dfMerged.head(10))
