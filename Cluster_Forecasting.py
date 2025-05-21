92edf06fd8e7c1d8f3ce1c04060bebfe655948f5
Twoje imię i nazwisko <w.daszysnki@outlook.com>
2025-05-21 11:26:17 +0200

    changed names

--- Cluster_Forecasting.py
+++ Cluster_Forecasting.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load data from Excel file into a DataFrame
-dfRawData = pd.read_excel(r"C:\Users\wiktor.daszynski\Downloads\Python\Claster Forecasting Project\Data Claster Forecasting.xlsx")
+dfRawData = pd.read_excel(r"C:\Users\wiktor.daszynski\Downloads\Python\Claster Forecasting Project\Data Cluster Forecasting.xlsx")
 # Display the first few rows of the DataFrame
 print(dfRawData.head(5))
 
